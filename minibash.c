6a6945280a88b8c64364b31c4e1da642fd21c098
root <root@DESKTOP-IG4J04R.>
2025-11-20 17:38:51 +0200

    added exec_program() which behaves same as system(), added wait for parent to wait chil finish execution first, added perror

--- minibash.c
+++ minibash.c
@@ -3,26 +3,62 @@
 #include <readline/history.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int execute_program(char *path, char **arg_vector, char **env_args)
+{
+        pid_t pid;
+
+        printf("\n Executing inside execute_program()");
+        pid = fork();
+
+        if (pid == -1)
+        {
+                perror("\n error happened while forking .. \n");
+        }
+        else if (pid == 0)
+        {
+                if (execve(path, arg_vector, env_args) == -1)
+                {
+                        perror("\n Error happened while executing execve()");
+                }
+        }
+        else
+        {
+                // printf("\n waiting for child to execute first");
+                int status;
+                waitpid(pid, &status, 0);
+                // printf("parent process : child finished with status %d \n", status);
+        }
+}
 
 int main()
 {
 
-        while (1)
+        char *prompt;
+        char target[] = "exit";
+
+        char path[] = "/bin/sh";
+        char *arg_vector[] = {"sh", "-c", prompt, NULL};
+        char *env_args[] = {NULL};
+
+        while ((prompt = readline("\n Enter CMD: ")) != NULL)
         {
+                printf("\n prompt ==>  %s", prompt);
+                add_history(prompt); // saves each line string, user can move through via arrow key or arrow key down
 
-                char *prompt;
-                char target[] = "clear";
+                arg_vector[2]=prompt;
+                
+                execute_program(path, arg_vector, env_args); // executes external program e.g. ls
 
-                while ((prompt = readline("\n Enter CMD: ")) != NULL)
+                free(prompt); // very important do not forget to clear to avoid memory leakage
+
+                if (strcmp(prompt, target) == 0)
                 {
-                        printf("\n prompt ==>  %s", prompt);
-                        add_history(prompt); // saves each line string, user can move through via arrow key or arrow key down
-                        free(prompt);        // very important do not forget to clear to avoid memory leakage
-
-                        if (strcmp(prompt, target) == 0)
-                        {
-                                rl_clear_history(); // clears working directory in linux
-                        }
+                        rl_clear_history(); // clears working directory in linux
                 }
         }
 
