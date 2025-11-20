f437e12264d67fb981e49dc4cd3b668b4ebbac00
root <root@DESKTOP-IG4J04R.>
2025-11-20 06:58:29 +0200

    Added add_history() ,adds lines each time and remembers them so we can go back to them using arrow keys, and rl_clear_history() when user says clear it forgets everything in history

--- minibash.c
+++ minibash.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <string.h>
 
 int main()
 {
 
-        printf("\nAsk me random questions:");
+        while (1)
+        {
 
-        char *prompt;
-        readline("Enter cmd : ");
+                char *prompt;
+                char target[] = "clear";
 
-        printf("Result of prompt is : %s",prompt);
+                while ((prompt = readline("\n Enter CMD: ")) != NULL)
+                {
+                        printf("\n prompt ==>  %s", prompt);
+                        add_history(prompt); // saves each line string, user can move through via arrow key or arrow key down
+
+                        if (strcmp(prompt, target) == 0)
+                        {
+                                rl_clear_history(); // clears working directory in linux
+                        }
+                }
+        }
 
         return 0;
 }
